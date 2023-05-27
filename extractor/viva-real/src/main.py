f94f54cb12d4f4605073b627a71300804f2a57dd
Victor Mattos <5757883+vicmattos@users.noreply.github.com>
2023-05-27 12:55:29 -0300

    feat: add request to get current data

--- extractor/viva-real/src/main.py
+++ extractor/viva-real/src/main.py
@@ -3,13 +3,19 @@ import pathlib
 import sys
 
 import bs4
+import requests
 
 
 def main():
-    with open("butanta-4000-3quartos.html") as fp:
-        soup = bs4.BeautifulSoup(fp, features="html.parser")
+    html = fetch_html()
+    soup = bs4.BeautifulSoup(html, features="html.parser")
     for div in soup.find_all(lambda tag: tag.get('id').isnumeric() if 'id' in tag.attrs else False):
         print(extract_values_from_div(div))
+
+
+def fetch_html() -> str:
+    resp = requests.get("https://www.vivareal.com.br/aluguel/sp/sao-paulo/zona-oeste/butanta/3-quartos/#com=sacada,quintal&quartos=3")
+    return resp.text
 
 
 def extract_values_from_div(div: bs4.element.Tag) -> tuple:
